5776d0dc3ead3d107086bc66c8289696bf43091f
Mario Lisbona <mario.lisbona@gmail.com>
2022-04-20 07:45:08 +1000

    Add comments

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
+//function to create random number between min and max
 function randomNumber (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function computerPlay() {
+    //use randomNumber to randomly choose between 1 - 3 and assign to rock, papper or scissors
     let randNum = randomNumber(1, 3);
+    //create variable to manipulate <p> to display computer choice
     const para = document.querySelector('#rpsComputer');
 
 
-    //assign result of randNum to compChoice = rock paper or scissors
+    //assign result of randNum to rock paper or scissors and display in rpsComputer <p>
     if (randNum === 1) {
         para.textContent = "Computer chose Rock";
         return "rock";
@@ -24,9 +27,10 @@ function computerPlay() {
 function personPlay(choice) {
     // create playerChoice variable to store the players choice
     let playerChoice = choice;
-
+    //create variable to manipulate <p> to display computer choice
     const para = document.querySelector('#rpsPlayer');
 
+    //display rock, paper or scissors in rpsPlayer <p>
     if (playerChoice === "rock") {
         para.innerText = "You chose: Rock";
         return choice;
@@ -41,11 +45,13 @@ function personPlay(choice) {
     
 
 function playRound(compSelection, playerSelection) {
-
+    //create variable to manipulate winner and score <p>
     const winner = document.querySelector('#winner');
     const score = document.querySelector('#score');
 
-    
+    //only choose a winner for 5 games
+    //increment playerScore or compScore depending on who wins. 
+    //update score <p>
     if (compScore < 5 && playerScore < 5) {
         //select first for combinations where the computer wins
         if ((compSelection === "rock" && playerSelection === "scissors") ||
@@ -60,8 +66,9 @@ function playRound(compSelection, playerSelection) {
                 score.textContent = `player: ${playerScore} | Computer: ${compScore}`;
             }
     }
-    console.log("playRound", compScore, playerScore);
-
+    //if either player or computer reach a score of 5
+    //call endGame() to disable buttons
+    //display winner
     if (playerScore == 5 || compScore == 5) {
         endGame();
         (playerScore == 5) ? winner.textContent = "You win, Computer looses!" : winner.textContent = "You loose, Computer wins!"
@@ -69,12 +76,15 @@ function playRound(compSelection, playerSelection) {
   
 }
 
+
+//function to disable RPS buttons
 function endGame () {
     paper.disabled = true;
     scissors.disabled = true;
     rock.disabled = true;
 }
 
+//function to reset the game
 function resetGame () {
     player = document.querySelector('#rpsPlayer');
     computer = document.querySelector('#rpsComputer');
@@ -90,30 +100,38 @@ function resetGame () {
     rock.disabled = false;
     compScore = 0;
     playerScore = 0;
-    console.log("resetGame()", compScore, playerScore);
 }
 
-
+//variables to maniuplate buttons and reset button
 const paper = document.getElementById('btnPaper')
 const scissors = document.getElementById('btnScissors')
 const rock = document.getElementById('btnRock')
 const playAgain = document.getElementById('reset');
-
 let compScore = 0;
 let playerScore = 0;
 
+
+
+//event listener for paper button.
+//calls playRound with 'paper' argument passed to personPlay function
 paper.addEventListener('click', () => {
     playRound(computerPlay(), personPlay('paper'))
 });
 
+//event listener for scissors button.
+//calls playRound with 'scissors' argument passed to personPlay function
 scissors.addEventListener('click', () => {
     playRound(computerPlay(), personPlay('scissors'))
 });
 
+//event listener for rock button.
+//calls playRound with 'rock' argument passed to personPlay function
 rock.addEventListener('click', () => {
     playRound(computerPlay(), personPlay('rock'))
 });
 
+//event listener for Play again button.
+//calls reset function
 playAgain.addEventListener('click', () => {
     resetGame()
 });
